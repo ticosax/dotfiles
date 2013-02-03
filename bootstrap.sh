b87c55c69c0666a3dd01d822a28cb51333b899be
ticosax <ticosax@free.fr>
2013-02-03 16:11:20 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
-sudo apt-get install exuberant-ctags python-fontforge
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+sudo apt-get install vim exuberant-ctags fontforge
 rm -f ~/.vimrc
 ln -s `pwd`/vimrc ~/.vimrc
 git clone https://github.com/Lokaltog/powerline-fonts.git ../powerline-fonts
@@ -7,3 +8,4 @@ cp ../powerline-fonts/Menlo/Menlo\ Regular\ for\ Powerline.otf ~/.fonts/
 fc-cache -vf ~/.fonts
 # TODO automatize this
 echo 'Configure your terminal with this font'
+vim +BundleInstall +qall
