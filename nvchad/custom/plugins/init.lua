5e206568da5685b75380d14f9035e085a672b986
Nicolas Delaby <nicolas.delaby@infarm.com>
2022-10-04 17:08:02 +0200

    Format and add some of the plugins I miss

--- nvchad/custom/plugins/init.lua
+++ nvchad/custom/plugins/init.lua
@@ -1,28 +1,29 @@
-local overrides = require "custom.plugins.overrides"
+local overrides = require("custom.plugins.overrides")
 
 return {
-  ["neovim/nvim-lspconfig"] = {
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.plugins.lspconfig"
-    end
-  },
-  ["nvim-treesitter/nvim-treesitter"] = {
-    override_options = overrides.treesitter,
-  },
+	["neovim/nvim-lspconfig"] = {
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.plugins.lspconfig")
+		end,
+	},
+	["nvim-treesitter/nvim-treesitter"] = {
+		override_options = overrides.treesitter,
+	},
 
+	["jose-elias-alvarez/null-ls.nvim"] = {
+		after = "nvim-lspconfig",
+		config = function()
+			require("custom.plugins.null-ls")
+		end,
+	},
 
-  ["jose-elias-alvarez/null-ls.nvim"] = {
-    after = "nvim-lspconfig",
-    config = function()
-      require "custom.plugins.null-ls"
-    end,
-  },
+	["tommcdo/vim-exchange"] = {},
 
-  ["tommcdo/vim-exchange"] = {},
-
-  ["williamboman/mason.nvim"] = {
-    override_options = overrides.mason.ensure_installed
-  },
-  ["nvim-lua/lsp-status.nvim"] = {}
+	["williamboman/mason.nvim"] = {
+		override_options = overrides.mason.ensure_installed,
+	},
+	["nvim-lua/lsp-status.nvim"] = {},
+	["tpope/vim-surround"] = {},
+	["psliwka/vim-smoothie"] = {},
 }
