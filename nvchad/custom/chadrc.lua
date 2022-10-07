32554899b079b27b2352c572c276f64f7e3c6e62
Nicolas Delaby <nicolas.delaby@infarm.com>
2022-10-07 10:36:44 +0200

    Readd my breakpoint shortcut

--- nvchad/custom/chadrc.lua
+++ nvchad/custom/chadrc.lua
@@ -1,10 +1,11 @@
 local M = {}
 
 M.ui = {
-  theme_toggle = { "ayu-dark", "one_light" },
-  theme = "ayu-dark",
+	theme_toggle = { "ayu-dark", "one_light" },
+	theme = "ayu-dark",
 }
 
-M.plugins = require "custom.plugins"
+M.plugins = require("custom.plugins")
+M.mappings = require("custom.mappings")
 
 return M
---
+++ nvchad/custom/mappings.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+M.general = {
+	n = {
+		["<leader>br"] = { "Obreakpoint()<esc>", "Insert python breakpoint" },
+	},
+}
+
+return M
