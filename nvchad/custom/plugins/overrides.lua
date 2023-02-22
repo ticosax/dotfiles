00dbf77307d3e4f7981a3b322989314f95384608
Nicolas Delaby <nicolas.delaby@infarm.com>
2023-02-22 12:51:08 +0100

    Formatting + ruff-lsp

--- nvchad/custom/plugins/overrides.lua
+++ nvchad/custom/plugins/overrides.lua
@@ -1,45 +1,43 @@
 local M = {}
 M.treesitter = {
-  ensure_installed = {
-    "vim",
-    "lua",
-    "python",
-    "markdown",
-    "rust",
-    "json",
-    "hcl",
-    "toml",
-  },
+	ensure_installed = {
+		"vim",
+		"lua",
+		"python",
+		"markdown",
+		"rust",
+		"json",
+		"hcl",
+		"toml",
+	},
 }
 M.mason = {
-  ensure_installed = {
-    "pylsp",
-    "rust_analyzer",
-    "pyright",
-    "graphql",
-    "tflint",
-    "yamlls",
-    "dockerls",
-    "terraformls",
-    "vimls",
-    "sumneko_lua",
-    "black",
-    "isort",
-    "flake8",
-    "pyproject-flake8",
+	ensure_installed = {
+		"pylsp",
+		"rust_analyzer",
+		"pyright",
+		"graphql",
+		"tflint",
+		"yamlls",
+		"dockerls",
+		"terraformls",
+		"vimls",
+		"sumneko_lua",
+		"black",
+		"pyproject-flake8",
+		"ruff-lsp",
 
-    -- lua stuff
-    "lua-language-server",
-    "stylua",
+		-- lua stuff
+		"lua-language-server",
+		"stylua",
 
-    -- web dev stuff
-    "typescript-language-server",
+		-- web dev stuff
+		"typescript-language-server",
 
-    "yamfmt",
-    "prettier",
-    "sytlua",
-    "sql-formatter",
-
-  },
+		"yamfmt",
+		"prettier",
+		"sytlua",
+		"sql-formatter",
+	},
 }
 return M
