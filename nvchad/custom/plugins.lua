bf5da2815a2943a0f09860d8879d1bdcd4388f13
Nicolas Delaby <nicolas.delaby@infarm.com>
2023-06-27 12:04:17 +0200

    I don't seem to be able to get rust-tools and nvim-dap-ui working together

--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -43,87 +43,63 @@ local plugins = {
 	{ "f-person/git-blame.nvim" },
 	{ "liuchengxu/vista.vim", lazy = false },
 	{
-		"simrat39/rust-tools.nvim",
+		"rcarriga/nvim-dap-ui",
+		ft = { "rust", "python", "go" },
 		config = function()
-			local rt = require("rust-tools")
-			rt.setup({
-				server = {
-					on_attach = function(_, bufnr)
-						-- Hover actions
-						-- vim.keymap.set("n", "<Leader>,", rt.hover_actions.hover_actions, { buffer = bufnr })
-						-- Code action groups
-						vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-					end,
-				},
-			})
+			require("dapui").setup()
 		end,
-		dependencies = { "mfussenegger/nvim-dap" },
-	},
-	{ "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap" } },
-	{
-		"mfussenegger/nvim-dap",
-		config = function()
-			local dap = require("dap")
-			dap.adapters.lldb = {
-				type = "executable",
-				command = "/usr/bin/lldb-vscode",
-				name = "lldb",
-			}
+		dependencies = {
+			{
+				"mfussenegger/nvim-dap",
+				config = function()
+					local dap = require("dap")
+					dap.adapters.lldb = {
+						type = "executable",
+						command = "/usr/bin/lldb-vscode",
+						name = "lldb",
+					}
+					dap.configurations.rust = {
+						{
+							name = "Rust debug",
+							type = "lldb",
+							request = "launch",
+							program = function()
+								vim.fn.jobstart("cargo build")
+								return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+							end,
+							cwd = "${workspaceFolder}",
+							stopOnEntry = false,
+							args = {},
+							showDisassembly = "never",
+							initCommands = function()
+								-- Find out where to look for the pretty printer Python module
+								local rustc_sysroot = vim.fn.trim(vim.fn.system("rustc --print sysroot"))
 
-			dap.adapters.codelldb = {
-				type = "server",
-				port = "${port}",
-				executable = {
-					command = "/usr/bin/codelldb",
-					args = { "--port", "${port}" },
-				},
-			}
-			dap.configurations.rust = {
-				{
-					name = "Rust debug",
-					type = "codelldb",
-					request = "launch",
-					program = function()
-						vim.fn.jobstart("cargo build")
-						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-					end,
-					cwd = "${workspaceFolder}",
-					stopOnEntry = false,
-					args = {},
-					showDisassembly = "never",
-					-- runInTerminal = true,
-					initCommands = function()
-						-- Find out where to look for the pretty printer Python module
-						local rustc_sysroot = vim.fn.trim(vim.fn.system("rustc --print sysroot"))
+								local script_import = 'command script import "'
+									.. rustc_sysroot
+									.. '/lib/rustlib/etc/lldb_lookup.py"'
+								local commands_file = rustc_sysroot .. "/lib/rustlib/etc/lldb_commands"
 
-						local script_import = 'command script import "'
-							.. rustc_sysroot
-							.. '/lib/rustlib/etc/lldb_lookup.py"'
-						local commands_file = rustc_sysroot .. "/lib/rustlib/etc/lldb_commands"
+								local commands = {}
+								local file = io.open(commands_file, "r")
+								if file then
+									for line in file:lines() do
+										table.insert(commands, line)
+									end
+									file:close()
+								end
+								table.insert(commands, 1, script_import)
 
-						local commands = {}
-						local file = io.open(commands_file, "r")
-						if file then
-							for line in file:lines() do
-								table.insert(commands, line)
-							end
-							file:close()
-						end
-						table.insert(commands, 1, script_import)
-
-						return commands
-					end,
-				},
-			}
-		end,
+								return commands
+							end,
+						},
+					}
+				end,
+			},
+		},
 	},
 	{ "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
 	{ "tpope/vim-repeat" },
-	{
-		"nvim-telescope/telescope-symbols.nvim",
-		-- config = function()
-		-- 	require("telescope.builtin").symbols({ sources = { "emoji" } })
-		-- end,
-	},
+	{ "nvim-telescope/telescope-symbols.nvim" },
 }
 return plugins
